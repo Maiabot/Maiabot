7483b151260007448e3be2e2e32ef23d02f1f845
maiabot <maiabot8@gmail.com>
2021-03-19 16:05:54 -0300

    First commit

---
+++ Maia-main/settings/config.js
@@ -0,0 +1,4 @@
+const config = {
+        botName: 'MAIA BOT 🤖',
+        ownerName: 'Maia'      
+}
---
+++ Maia-main/src/maiamenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Maia 🥵*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.maiamenu = maiamenu
